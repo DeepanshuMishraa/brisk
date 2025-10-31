4f78a4df3b90ef67232d66622ef6e9eea0056270
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-10-31 12:12:42 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen text-black bg-white">
+      Hello world
+    </div>
+  );
+}
